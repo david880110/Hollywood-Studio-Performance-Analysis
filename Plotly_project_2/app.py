9d6a71fa65eb3da894d7c8036a6818d37097b202
Lindsay <lindsayyan0311@gmail.com>
2018-06-17 11:19:01 -0700

    Rotten tomato and oscar plots

--- Plotly_project_2/app.py
+++ Plotly_project_2/app.py
@@ -18,8 +18,18 @@ def sales():
     sales = pd.read_csv('Box_office_chart.csv')
     return jsonify(sales.to_dict(orient="records"))
 
+@app.route('/score')
+def score():
+
+    sales = pd.read_csv('Rotten_Tomato_chart.csv')
+    return jsonify(sales.to_dict(orient="records"))
 
 
+@app.route('/oscar')
+def oscar():
+
+    sales = pd.read_csv('Oscar_chart.csv')
+    return jsonify(sales.to_dict(orient="records"))
 
 
 if __name__ == "__main__":
