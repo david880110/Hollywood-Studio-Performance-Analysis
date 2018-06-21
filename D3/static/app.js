ba6c9b7973536e9c4dd68e469cbf7f03ce469ff1
Sonia Yang <soniayangcs@gmail.com>
2018-06-20 21:52:46 -0700

    restructured some functions to be cleaner

--- D3/static/app.js
+++ D3/static/app.js
@@ -1,4 +1,4 @@
-//dropdown list
+//dropdown list for all the years
 function dropdownList() {
     
     //data route for dropdown labels
@@ -6,7 +6,12 @@ function dropdownList() {
     
     // Grab a reference to the dropdown select element
     var selectYear = document.getElementById('selectYear');
-    //console.log(selectYear);
+    
+    //default option on load
+    var defaultOption = document.createElement('option');
+    defaultOption.text = 'all_years';
+    defaultOption.value = 'all_years';
+    selectYear.appendChild(defaultOption);
     
     // Use the list of years from the route to populate the select options
     Plotly.d3.json(years_url, function(error, response) {
@@ -26,6 +31,7 @@ function dropdownList() {
 //call to load in the list
 dropdownList();
 
+//change pie charts based off dropdown list
 function optionChanged(year) {
     
     var isEmpty = document.getElementById('pieChart').innerHTML === "";
@@ -39,177 +45,89 @@ function optionChanged(year) {
         pieChart.innerHTML = '';
         oscars_by_year(year);
     }
-}
-    
-//hardcoded due to time constraints
+}   
+
+//Oscars pie charts selected by year
+//Hardcoded due to time constraints
+//not an ideal solution but manageable with this dataset
 function oscars_by_year(year) {
     switch(year) {
         case '1990':
-            oscars_1990();
-            console.log(year);
+            oscars_chart("1990", 1,0,2,1,0,2);
             break;
-        case '1991':
-            oscars_1991();
-            console.log(year);    
+        case '1991': 
+            oscars_chart("1991", 1,0,0,2,0,1)
             break;
         case '1992':
-            oscars_1992();
-            console.log(year);
+            oscars_chart("1992", 1,2,0,4,1,1)
             break;
+        case '1993':
+            break;
+        case '1994':
+            oscars_chart("1994", 1,0,2,1,0,2);
+            break;
+        case '1995': 
+            oscars_chart("1995", 1,0,2,1,0,2)
+            break;
+        case '1996':
+            oscars_chart("1996", 1,0,2,1,0,2)
+            break;
+        case '1997':
+            break;
+        case '1998':
+            oscars_chart("1998", 1,0,2,1,0,2);
+            break;
+        case '1999': 
+            oscars_chart("1999",1,0,2,1,0,2)
+            break;
+        case '2000':
+            oscars_chart("2000",1,0,2,1,0,2)
+            break;
+        
         default:
-            oscars_summary();
+            //console.log(year);
+            oscars_chart("1990-2017",24,22,19,25,27,33);
     }
 }
 
 
-//oscars_summary();
-
-//Oscars pie charts
-//Hardcoded due to time constraints
-
-//pie chart for the overall summary of Oscar winners
-function oscars_summary() { 
-    oscars_summary_data = [
+function oscars_chart (year, bv_value, fox_value, paramount_value, sony_value, universal_value, wb_value) {
+    data = [
 			{
 				"label": "BV",
-				"value": 24,
+				"value": parseInt(bv_value),
 				"color": "#2484c1"
 			},
 			{
 				"label": "Fox",
-				"value": 22,
+				"value": parseInt(fox_value),
 				"color": "#0c6197"
 			},
 			{
 				"label": "Paramount",
-				"value": 19,
+				"value": parseInt(paramount_value),
 				"color": "#4daa4b"
 			},
 			{
 				"label": "Sony",
-				"value": 25,
+				"value": parseInt(sony_value),
 				"color": "#90c469"
 			},
 			{
 				"label": "Universal",
-				"value": 27,
+				"value": parseInt(universal_value),
 				"color": "#daca61"
 			},
 			{
 				"label": "WB",
-				"value": 33,
-				"color": "#e4a14b"
-			}
-		]
-    
-    var pie = new d3pie("overallPieChart", {
-	"header": {
-		"title": {
-			"text": "Oscar Winners by Studio",
-			"fontSize": 24,
-			"font": "open sans"
-		},
-		"subtitle": {
-            "text": "Overall Summary of Winners from 1990-2017",
-			"color": "#999999",
-			"fontSize": 12,
-			"font": "open sans"
-		},
-		"titleSubtitlePadding": 9
-	},
-	"footer": {
-		"color": "#999999",
-		"fontSize": 10,
-		"font": "open sans",
-		"location": "bottom-left"
-	},
-	"size": {
-		"canvasWidth": 590,
-		"pieOuterRadius": "90%"
-	},
-	"data": {
-		"sortOrder": "value-desc",
-		"content": oscars_summary_data
-	},
-	"labels": {
-		"outer": {
-			"pieDistance": 32
-		},
-		"inner": {
-			"hideWhenLessThanPercentage": 3
-		},
-		"mainLabel": {
-			"fontSize": 11
-		},
-		"percentage": {
-			"color": "#ffffff",
-			"decimalPlaces": 0
-		},
-		"value": {
-			"color": "#adadad",
-			"fontSize": 11
-		},
-		"lines": {
-			"enabled": true
-		},
-		"truncation": {
-			"enabled": true
-		}
-	},
-	"effects": {
-		"pullOutSegmentOnClick": {
-			"effect": "linear",
-			"speed": 400,
-			"size": 8
-		}
-	},
-	"misc": {
-		"gradient": {
-			"enabled": true,
-			"percentage": 100
-		}
-	}
-});
-}
-
-//1990
-function oscars_1990() { 
-    oscars_1990_data = [
-			{
-				"label": "BV",
-				"value": 1,
-				"color": "#2484c1"
-			},
-			{
-				"label": "Fox",
-				"value": 0,
-				"color": "#0c6197"
-			},
-			{
-				"label": "Paramount",
-				"value": 2,
-				"color": "#4daa4b"
-			},
-			{
-				"label": "Sony",
-				"value": 1,
-				"color": "#90c469"
-			},
-			{
-				"label": "Universal",
-				"value": 0,
-				"color": "#daca61"
-			},
-			{
-				"label": "WB",
-				"value": 2,
+				"value": parseInt(wb_value),
 				"color": "#e4a14b"
 			}
 		]
     var pie = new d3pie("pieChart", {
 	"header": {
 		"title": {
-			"text": "Oscar Winners by Studio (1990)",
+			"text": "Oscar Winners by Studio (" + year + ")",
 			"fontSize": 24,
 			"font": "open sans"
 		},
@@ -226,7 +144,7 @@ function oscars_1990() {
 	},
 	"data": {
 		"sortOrder": "value-desc",
-		"content": oscars_1990_data
+		"content": data
 	},
 	"labels": {
 		"outer": {
@@ -269,201 +187,3 @@ function oscars_1990() {
 });
 }
 
-//1991
-function oscars_1991() { 
-    oscars_1991_data = [
-			{
-				"label": "BV",
-				"value": 1,
-				"color": "#2484c1"
-			},
-			{
-				"label": "Fox",
-				"value": 0,
-				"color": "#0c6197"
-			},
-			{
-				"label": "Paramount",
-				"value": 0,
-				"color": "#4daa4b"
-			},
-			{
-				"label": "Sony",
-				"value": 2,
-				"color": "#90c469"
-			},
-			{
-				"label": "Universal",
-				"value": 0,
-				"color": "#daca61"
-			},
-			{
-				"label": "WB",
-				"value": 1,
-				"color": "#e4a14b"
-			}
-		]
-    var pie = new d3pie("pieChart", {
-	"header": {
-		"title": {
-			"text": "Oscar Winners by Studio (1991)",
-			"fontSize": 24,
-			"font": "open sans"
-		},
-	},
-	"footer": {
-		"color": "#999999",
-		"fontSize": 10,
-		"font": "open sans",
-		"location": "bottom-left"
-	},
-	"size": {
-		"canvasWidth": 590,
-		"pieOuterRadius": "90%"
-	},
-	"data": {
-		"sortOrder": "value-desc",
-		"content": oscars_1991_data
-	},
-	"labels": {
-		"outer": {
-			"pieDistance": 32
-		},
-		"inner": {
-			"hideWhenLessThanPercentage": 3
-		},
-		"mainLabel": {
-			"fontSize": 11
-		},
-		"percentage": {
-			"color": "#ffffff",
-			"decimalPlaces": 0
-		},
-		"value": {
-			"color": "#adadad",
-			"fontSize": 11
-		},
-		"lines": {
-			"enabled": true
-		},
-		"truncation": {
-			"enabled": true
-		}
-	},
-	"effects": {
-		"pullOutSegmentOnClick": {
-			"effect": "linear",
-			"speed": 400,
-			"size": 8
-		}
-	},
-	"misc": {
-		"gradient": {
-			"enabled": true,
-			"percentage": 100
-		}
-	}
-});
-}
-
-//1992
-function oscars_1992() { 
-    oscars_1992_data = [
-			{
-				"label": "BV",
-				"value": 1,
-				"color": "#2484c1"
-			},
-			{
-				"label": "Fox",
-				"value": 2,
-				"color": "#0c6197"
-			},
-			{
-				"label": "Paramount",
-				"value": 0,
-				"color": "#4daa4b"
-			},
-			{
-				"label": "Sony",
-				"value": 4,
-				"color": "#90c469"
-			},
-			{
-				"label": "Universal",
-				"value": 1,
-				"color": "#daca61"
-			},
-			{
-				"label": "WB",
-				"value": 1,
-				"color": "#e4a14b"
-			}
-		]
-    var pie = new d3pie("pieChart", {
-	"header": {
-		"title": {
-			"text": "Oscar Winners by Studio (1992)",
-			"fontSize": 24,
-			"font": "open sans"
-		},
-	},
-	"footer": {
-		"color": "#999999",
-		"fontSize": 10,
-		"font": "open sans",
-		"location": "bottom-left"
-	},
-	"size": {
-		"canvasWidth": 590,
-		"pieOuterRadius": "90%"
-	},
-	"data": {
-		"sortOrder": "value-desc",
-		"content": oscars_1992_data
-	},
-	"labels": {
-		"outer": {
-			"pieDistance": 32
-		},
-		"inner": {
-			"hideWhenLessThanPercentage": 3
-		},
-		"mainLabel": {
-			"fontSize": 11
-		},
-		"percentage": {
-			"color": "#ffffff",
-			"decimalPlaces": 0
-		},
-		"value": {
-			"color": "#adadad",
-			"fontSize": 11
-		},
-		"lines": {
-			"enabled": true
-		},
-		"truncation": {
-			"enabled": true
-		}
-	},
-	"effects": {
-		"pullOutSegmentOnClick": {
-			"effect": "linear",
-			"speed": 400,
-			"size": 8
-		}
-	},
-	"misc": {
-		"gradient": {
-			"enabled": true,
-			"percentage": 100
-		}
-	}
-});
-}
-
-
-//oscars_1990();
-//oscars_1991();
-//oscars_1992();
